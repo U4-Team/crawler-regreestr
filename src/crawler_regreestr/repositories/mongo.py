5fd844891b197b1fbb4665acfad05c36b6622c7d
Sergey Volkov <volkov.sergey.e@gmail.com>
2018-12-09 16:03:13 +0300

    Fix

--- src/crawler_regreestr/repositories/mongo.py
+++ src/crawler_regreestr/repositories/mongo.py
@@ -5,6 +5,9 @@ from core.repositories import BaseMongoRepository
 
 class MongoRepository(BaseMongoRepository):
     def store_company(self, company: Dict):
+        if 'INN' not in company:
+            self.logger.warning('Skip: %s', company)
+            return
         self._client.crawlerRegreestr.companies.replace_one(
             {'INN': company['INN']},
             company,
